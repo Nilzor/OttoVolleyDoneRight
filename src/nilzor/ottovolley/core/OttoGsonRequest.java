3c0ee5a1d01dbba71333c7bbedfc7e9e486b6c6a
Frode Nilsen <nilzor@gmail.com>
2014-05-16 10:44:32 +0200

    Comment clarification for Request ID

--- src/nilzor/ottovolley/core/OttoGsonRequest.java
+++ src/nilzor/ottovolley/core/OttoGsonRequest.java
@@ -5,9 +5,10 @@ import nilzor.ottovolley.volleyextensions.GsonRequest;
 
 /** GsonRequest which passes the result on to an Otto Message Bus */
 public class OttoGsonRequest<T> extends GsonRequest<T> {
+    /** Request ID counter for this session */
     private static int _idCounter = 1;
 
-    /** A session-unique ID for this request. Reset when app process is killed. */
+    /** A ID for this request, unique for the lifetime of the process (given that you do < 2BN requests) */
     public int requestId;
 
     public OttoGsonRequest(Bus eventBus, String url, Class<T> classType) {
